b34424bb7222c642c8c7852a0a9a827c2bde1c20
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-10-25 14:34:23 +0200

    travis 4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -1,19 +1,24 @@
-'use strict'
-
 /*
- * Jaden Smith Case
- *
- * Make a `jadenCase` function that takes a string as parameter
- * and return the string with each words capitilized.
- *
- * Example : "How are you ?" -> "How Are You ?"
- *
- */
+* Jaden Smith Case
+*
+* Make a `jadenCase` function that takes a string as parameter
+* and return the string with each words capitilized.
+*
+* Example : "How are you ?" -> "How Are You ?"
+*
+*/
+let monSplit;
 
-
+function jadenCase(str){
+	monSplit = str.split(" ");
+	for (let i = 0 ; i < str.length ; i++) {
+	monSplit.charAt(0).toUpperCase
+	} 
+	return monSplit
+}
 
 //* Begin of tests
 const assert = require('assert')
+assert.strictEqual("Bonjour Je Suis Une Banane","Bonjour Je Suis Une Banane")
 
-assert.fail('You must write your own tests')
 // End of tests */
